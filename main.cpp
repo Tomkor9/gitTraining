34dc4148eb7892152a7ba1098d40ecfff504d377
Tomasz K <94.sham@gmail.com>
2018-07-07 17:46:28 +0200

    interface changes

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main()
 {
     entity * edek = new entity("edek");
     entity * fredek = new entity("fredek", 1.13, 9.9);
+    std::cout<<"Objects log:\n";
     edek->show();
     fredek->show();
+    std::cout<<"End of a log!\n";
     return 0;
 }
