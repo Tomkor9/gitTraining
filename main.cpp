8e8b629aff6a5727a1be2a477fca3985ddb87676
Tomasz K <94.sham@gmail.com>
2018-07-07 17:21:33 +0200

    commit two - edek changed

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    entity * edek = new entity();
+    entity * edek = new entity("edek");
     entity * fredek = new entity("fredek", 1.13, 9.9);
     edek->show();
     fredek->show();
