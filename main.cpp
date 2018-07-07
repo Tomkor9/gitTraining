f09dc453a5f2192748cfd8cd21d32ea936730508
Tomasz K <94.sham@gmail.com>
2018-07-07 17:51:07 +0200

    if added

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 
 int main()
 {
+    int a = 0;
     entity * edek = new entity("edek");
     entity * fredek = new entity("fredek", 1.13, 9.9);
     edek->show();
     fredek->show();
+    if (a == 0)
+    std::cout<<"Zmienna a wynosi zero!\n";
     return 0;
 }
