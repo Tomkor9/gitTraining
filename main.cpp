e837af213c67bae22fa82aaab9edc970f8581d86
Tomasz K <94.sham@gmail.com>
2018-07-08 20:53:54 +0200

    Revert "if added"
    This reverts commit f09dc453a5f2192748cfd8cd21d32ea936730508.

--- main.cpp
+++ main.cpp
@@ -3,13 +3,10 @@
 
 int main()
 {
-    int a = 0;
     entity * edek = new entity("edek");
     entity * fredek = new entity("fredek", 1.13, 9.9);
     std::cout<<"Objects log:\n";
     edek->show();
     fredek->show();
-    if (a == 0)
-    std::cout<<"Zmienna a wynosi zero!\n";
     return 0;
 }
